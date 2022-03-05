1c6c066c90470221f047a160aed881f9ab6b9d7e
Stévillis Sousa <stevillis@hotmail.com>
2022-03-05 14:55:45 -0400

    Properties and Spread Operator

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import "./App.css";
-import { MeuComponente, MeusComponentes } from "./components/MeuComponente";
+import { MeuComponente } from "./components/MeuComponente";
 
 function App() {
-  const nome = "MeuComponente3";
-  const MeuComponente3 = MeusComponentes[nome];
+  const dados = {
+    nome: "Laudon",
+    sobrenome: "Jordan",
+    idade: 28
+  }
   return (
-    <>
-      <MeuComponente nome="Jordan Laudon" />
-      <MeusComponentes.MeuComponente2 />
-      <MeuComponente3 nome="Laudon Jordan" />
-    </>
+    <MeuComponente
+      {...dados}
+    />
   );
 }
 
--- src/components/MeuComponente.js
+++ src/components/MeuComponente.js
@@ -1,29 +1,17 @@
 import React, { Component } from "react";
 
-/*
-function MeuComponente() {
-    return <h1>Hello, world!</h1>
-}
-
-const MeuComponente2 = () => {
-    return <h2>Welcome!</h2>
-}
-*/
 
 class MeuComponente extends Component {
-  render() {
-    const { props } = this;
-    return <h1>Hello, {props.nome}!</h1>;
-  }
+    render() {
+        const { props } = this;
+        return (
+            <ul>
+                <li>Nome: {props.nome}</li>
+                <li>Sobrenome: {props.sobrenome}</li>
+                <li>Idade: {props.idade}</li>
+            </ul>
+        );
+    }
 }
-
-export const MeusComponentes = {
-  MeuComponente2: function () {
-    return <h2>Welcome!</h2>;
-  },
-  MeuComponente3: function (props) {
-    return <span>Raça, {props.nome}</span>;
-  },
-};
 
 export { MeuComponente };
