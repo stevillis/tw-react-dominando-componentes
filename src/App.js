fafc3f9fcf00b77d05870a57ede050598901b737
Stévillis Sousa <stevillis@hotmail.com>
2022-03-05 12:42:54 -0400

    Create Component with Class

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
-import { MeuComponente, MeuComponente2 } from './components/MeuComponente';
+import { MeuComponente } from './components/MeuComponente';
 
 function App() {
   return (
     <>
       <MeuComponente />
-      <MeuComponente2 />
     </>
   );
 }
--- src/components/MeuComponente.js
+++ src/components/MeuComponente.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 
+/*
 function MeuComponente() {
     return <h1>Hello, world!</h1>
 }
@@ -8,5 +9,12 @@ function MeuComponente() {
 const MeuComponente2 = () => {
     return <h2>Welcome!</h2>
 }
+*/
 
-export { MeuComponente, MeuComponente2 };
+class MeuComponente extends Component {
+    render() {
+        return <h1>Hello, world!</h1>
+    }
+}
+
+export { MeuComponente };
