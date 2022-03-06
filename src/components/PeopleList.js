7336717c9e268a191c396466e7f8a28da4cc574e
Stévillis Sousa <stevillis@hotmail.com>
2022-03-05 21:45:41 -0400

    Keys

--- src/components/PeopleList.js
+++ src/components/PeopleList.js
@@ -3,14 +3,17 @@ import React, { Component } from 'react';
 
 const data = [
     {
+        id: 1,
         nome: "Ana Cláudia",
         idade: 18
     },
     {
+        id: 8,
         nome: "Roberto Cunha",
         idade: 19
     },
     {
+        id: 13,
         nome: "Joscicleia",
         idade: 21
     }
@@ -20,9 +23,11 @@ class PeopleList extends Component {
     render() {
         return (
             <ul>
-                {data.map(person =>
-                    <li>Nome: {person.nome}, Idade: {person.idade} anos</li>
-                )}
+                {
+                    data.map(person =>
+                        <li key={person.id}>Nome: {person.nome}, Idade: {person.idade} anos</li>
+                    )
+                }
             </ul>
         );
     }
