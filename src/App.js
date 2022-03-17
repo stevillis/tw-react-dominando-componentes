0ddb17fd4ac7efdc36b67aea1bfd463502b1cce3
Stévillis Sousa <stevillis@hotmail.com>
2022-03-16 21:40:44 -0400

    Try to switch between elements

--- src/App.js
+++ src/App.js
@@ -1,20 +1,55 @@
+import React, { Component } from "react";
 import "./App.css";
 import ClickList from "./components/ClickList";
 import ClickListItem from "./components/ClickListItem";
 import MyVideo from "./components/MyVideo";
+import Time from './components/Time';
 
 
-function App() {
-  return (
-    <div>
-      <MyVideo src="https://ak.picdn.net/shutterstock/videos/1048055704/preview/stock-footage-mother-multi-tasking-holding-baby-infant-and-using-computer-laptop-at-home-candid-authentic-and.webm" />
-      <ClickList>
-        <ClickListItem></ClickListItem>
-        <ClickListItem></ClickListItem>
-        <ClickListItem></ClickListItem>
-      </ClickList>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.item1 = React.createRef();
+    this.item2 = React.createRef();
+
+    this.state = {
+      selectedItem: this.item1
+    }
+
+    this.toggleItem = this.toggleItem.bind(this);
+  }
+
+  toggleItem() {
+    this.setState(({ selectedItem }) => {
+      return {
+        selectedItem: (selectedItem === this.item1 ? this.item2 : this.item1)
+      }
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div>
+          <ul>
+            <li ref={this.item1}>
+              {this.state.selectedItem === this.item1 ? <Time /> : ''}
+            </li>
+            <li ref={this.item2}>
+              {this.state.selectedItem === this.item2 ? <Time /> : ''}
+            </li>
+          </ul>
+          <button onClick={this.toggleItem}>Toggle</button>
+        </div>
+        <MyVideo src="https://ak.picdn.net/shutterstock/videos/1048055704/preview/stock-footage-mother-multi-tasking-holding-baby-infant-and-using-computer-laptop-at-home-candid-authentic-and.webm" />
+        <ClickList>
+          <ClickListItem></ClickListItem>
+          <ClickListItem></ClickListItem>
+          <ClickListItem></ClickListItem>
+        </ClickList>
+      </div >
+    );
+  }
 }
 
 export default App;
